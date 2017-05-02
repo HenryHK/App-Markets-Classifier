cafa122c231c8c299ea4bf38d76ede88475566fc
lhan <henryhan.hku@gmail.com>
2017-05-02 21:42:39 +1000

    add nbclassifier

---
+++ NBClassifier.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python3
+
+import csv
+from operator import itemgetter
+
+import numpy as np
+
+
+def concentrateData(train_data, labeled_data):
+    training_list = list(csv.reader(open(train_data,'r'), delimiter=','))
+    labeled_list = list(csv.reader(open(labeled_data,'r'), delimiter=','))
+    sorted_training = sorted(training_list, key=itemgetter(0))
+    sorted_labeled = sorted(labeled_list, key=itemgetter(0))
+    i = 0
+    for data in sorted_training:
+        data.insert(1,sorted_labeled[i][1])
+        # print(data)
+        i = i+1
+    return sorted_training
+
+def labelCount(labeled_data):
+    labeled_list = list(csv.reader(open(labeled_data,'r'), delimiter=','))
+    labelDict = {}
+    for data in labeled_list:
+        # print(data[1])
+        if data[1] in labelDict.keys():
+            labelDict[data[1]] = labelDict[data[1]]+1
+        else:
+            labelDict[data[1]] = 1
+    return labelDict
+
+
+def getLabelProb(labeled_training_list, labelDict):
+    labelProbDict = {}
+    for label in labelDict.keys():
+        labelProbDict[label]  = np.zeros(13626)
+    for data in labeled_training_list:
+        if data[1] in labelDict.keys():
+            for index in range(13626):
+                if float(data[index+2]) > 0:
+                    labelProbDict[label][index] += 1
+    for label in labelDict.keys():
+        labelProbDict[label] = labelProbDict[label]/labelDict[label]
+    return labelProbDict
+
+def readTestData(test_file):
+    test_data = list(csv.reader(open(test_file,'r'), delimiter=','))
+    return test_data
+
+def run():
+    pass
+
+labeled_training_list = concentrateData("assignment1_2017S1/training_data.csv", "assignment1_2017S1/training_labels.csv")
+labelDict = labelCount("assignment1_2017S1/training_labels.csv")
+labelProbDict = getLabelProb(labeled_training_list, labelDict)
+
+
+i = 0
+test_data = []
+for data in labeled_training_list:
+    test_data.append(data)
+    i += 1
+    if(i==1000):
+        break
+
+
+result = 0
+for data in test_data:
+    probDict = {}
+    for label in labelProbDict.keys():
+        prob = 1.0
+        for index in range(13626):
+            if float(data[index+2])>0:
+                prob *= labelProbDict[label][index]
+            else:
+                prob *= (1-labelProbDict[label][index])
+        probDict[label] = prob*(labelDict[label]/20104)
+    label = max(probDict.items(), key=itemgetter(1))[0]
+    if(label == data[1]):
+        result += 1
+
+print(result)
